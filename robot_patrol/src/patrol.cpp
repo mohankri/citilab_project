233cea0f213165c9fb8a95f0eb96c0a4cabac81d
Krishna Mohan <mohankri@gmail.com>
2026-07-16 06:33:33 +0000

    handle latency lag for obstacle detection

--- robot_patrol/src/patrol.cpp
+++ robot_patrol/src/patrol.cpp
@@ -52,7 +52,9 @@ private:
     float direction_ = 0;
     bool  obstacle_ahead_ = false;
 
-    static constexpr float OBSTACLE_THRESHOLD = 0.35f; // metres
+    //static constexpr float OBSTACLE_THRESHOLD = 0.35f; // metres
+    static constexpr float OBSTACLE_THRESHOLD = 0.70f; // metres
+
     static constexpr float HALF_FOV  = M_PI / 18.0f; // 20
     //static constexpr float HALF_FOV = M_PI; /// 3.0f;  //30
     //static constexpr float HALF_FOV = M_PI;
